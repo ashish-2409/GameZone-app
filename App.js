935759bdc7569f22a97fae55a08e0d0d637be5dc
Ashish Trehan <ashishtrehan002@gmail.com>
2023-03-21 20:14:55 +0530

    added react navigation setup

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font'
 import Home from './screens/home'
 import AppLoading from 'expo-app-loading'
+import Navigator from './routes/homeStack'
 
 const getFonts = () => {
   return Font.loadAsync({
@@ -16,7 +17,7 @@ export default function App() {
 
   if (fontsLoaded) {
     return (
-      <Home></Home>
+      <Navigator/>
     );
   }
   else {
